6a255619d4e92d36b3bf982c9577d6e8a7963731
BooksCup <812809680@qq.com>
2020-02-28 10:13:11 +0800

    完善部分接口代码

--- src/main/java/com/bc/rm/server/controller/UserController.java
+++ src/main/java/com/bc/rm/server/controller/UserController.java
@@ -38,6 +38,15 @@ public class UserController {
         return new ResponseEntity<>(pageInfo, HttpStatus.OK);
     }
 
+    /**
+     * 创建用户
+     *
+     * @param name  用户名
+     * @param phone 手机号
+     * @param mail  邮箱
+     * @param desc  用户描述
+     * @return ResponseEntity<String>
+     */
     @ApiOperation(value = "创建用户", notes = "创建用户")
     @PostMapping(value = "")
     public ResponseEntity<User> addUser(
@@ -45,10 +54,18 @@ public class UserController {
             @RequestParam String phone,
             @RequestParam String mail,
             @RequestParam(required = false) String desc) {
-        logger.info("name: " + name + ", phone: " + phone + ", mail: " + mail + ", desc: " + desc);
-        User user = new User(name, phone, mail, desc);
-        userService.addUser(user);
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        logger.info("[addUser] name: " + name + ", phone: " + phone
+                + ", mail: " + mail + ", desc: " + desc);
+        ResponseEntity<User> responseEntity;
+        try {
+            User user = new User(name, phone, mail, desc);
+            userService.addUser(user);
+            responseEntity = new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("addUser error. errorMsg: " + e.getMessage());
+            responseEntity = new ResponseEntity<>(new User(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
     }
 
     /**
@@ -89,11 +106,20 @@ public class UserController {
             @RequestParam String status,
             @RequestParam(required = false) String desc) {
         logger.info("[updateUser] userId: " + userId + ", status: " + status + ", desc:" + desc);
-        User user = new User();
-        user.setId(userId);
-        user.setStatus(status);
-        user.setDesc(desc);
-        userService.updateUser(user);
-        return new ResponseEntity<>("", HttpStatus.OK);
+        ResponseEntity<String> responseEntity;
+        try {
+            User user = new User();
+            user.setId(userId);
+            user.setStatus(status);
+            user.setDesc(desc);
+            userService.updateUser(user);
+            responseEntity = new ResponseEntity<>(ResponseMsg.UPDATE_USER_SUCCESS.getResponseCode(),
+                    HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error("updateUser error. errorMsg: " + e.getMessage());
+            responseEntity = new ResponseEntity<>(ResponseMsg.UPDATE_USER_ERROR.getResponseCode(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return responseEntity;
     }
 }
--- src/main/java/com/bc/rm/server/enums/ResponseMsg.java
+++ src/main/java/com/bc/rm/server/enums/ResponseMsg.java
@@ -9,9 +9,11 @@ public enum ResponseMsg {
     /**
      * 接口返回信息
      */
+    UPDATE_USER_SUCCESS("UPDATE_USER_SUCCESS", "编辑用户成功"),
+    UPDATE_USER_ERROR("UPDATE_USER_ERROR", "编辑用户失败"),
 
-    DELETE_USER_SUCCESS("DELETE_USER_SUCCESS", "删除好友成功"),
-    DELETE_USER_ERROR("DELETE_USER_ERROR", "删除好友成功");
+    DELETE_USER_SUCCESS("DELETE_USER_SUCCESS", "删除用户成功"),
+    DELETE_USER_ERROR("DELETE_USER_ERROR", "删除用户失败");
 
     ResponseMsg(String responseCode, String responseMessage) {
         this.responseCode = responseCode;
